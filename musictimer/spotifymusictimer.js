3c24655e731885473d44c16af6055e81c4fbe420
Colin <colintoft@icloud.com>
2022-04-20 13:54:22 -0400

    Used active class to highlight the currently playing track in blue

--- musictimer/spotifymusictimer.js
+++ musictimer/spotifymusictimer.js
@@ -841,25 +841,21 @@ const UIController = (function() {
 
     var backgroundOpacity = 1.0;
     
-    function _boldTrack(id) {
+    function _makeTrackActive(id) {
         if (document.getElementById(id) !== null) { 
-            document.getElementById(id).style.backgroundColor = '#e0e0e0';
-            document.getElementById(id).style.fontWeight = 500;
-            document.getElementById(id).style.color = '#000000';
+            document.getElementById(id).classList.add("active");
         }
     }
     
-    function _unBoldTrack(id) {
+    function _makeTrackUnactive(id) {
         if (document.getElementById(id) !== null) { 
-            document.getElementById(id).style.backgroundColor = null;
-            document.getElementById(id).style.fontWeight = null;
-            document.getElementById(id).style.color = null;
+            document.getElementById(id).classList.remove("active");
             
-            var t = document.getElementById(id).innerHTML;
+            var t = document.getElementById(id).querySelector("#trackNameAndDuration").querySelector("#trackDuration").innerHTML;
             if (t.lastIndexOf("/") !== -1 && t.lastIndexOf("/") > t.lastIndexOf("(")) {
                 t = t.substr(0, t.lastIndexOf("(") + 1) + t.substr(t.lastIndexOf("/") + 1);
             }
-            document.getElementById(id).innerHTML = t;
+            document.getElementById(id).querySelector("#trackNameAndDuration").querySelector("#trackDuration").innerHTML = t;
         }
     }
     
@@ -868,8 +864,8 @@ const UIController = (function() {
             return false;
         }
         
-        _unBoldTrack(currentTrack);
-        _boldTrack(id);
+        _makeTrackUnactive(currentTrack);
+        _makeTrackActive(id);
         currentTrack = id;
         return true;
     }
@@ -879,7 +875,7 @@ const UIController = (function() {
             return;
         }
         if (document.getElementById(currentTrack)) {
-            var t = document.getElementById(currentTrack).innerHTML;
+            var t = document.getElementById(id).querySelector("#trackNameAndDuration").querySelector("#trackDuration").innerHTML;
             
             if (t.lastIndexOf("/") !== -1 && t.lastIndexOf("/") > t.lastIndexOf("(")) {
                 t = t.substr(0, t.lastIndexOf("(") + 1) + progress + t.substr(t.lastIndexOf("/"));
@@ -887,7 +883,7 @@ const UIController = (function() {
                 t = t.substr(0, t.lastIndexOf("(") + 1) + progress + "/" + t.substr(t.lastIndexOf("(") + 1);
             }
             
-            document.getElementById(currentTrack).innerHTML = t;
+            document.getElementById(id).querySelector("#trackNameAndDuration").querySelector("#trackDuration").innerHTML = t;
         }
     }
     
@@ -939,14 +935,14 @@ const UIController = (function() {
         },
         createTrack(id, index, name, artist, length) {
             const html = '<a href="#" class="list-group-item list-group-item-action" id="' + id + '">' + 
-                '<div class="d-flex w-100 justify-content-between">' +
-                    '<h5 class="mb-1">' + name + '</h5>' +
-                    '<small>' + length.toHHMMSS() + '</small>' +
+                '<div id="trackNameAndDuration" class="d-flex w-100 justify-content-between">' +
+                    '<h6 class="mb-1">' + name + '</h6>' +
+                    '<small id="trackDuration">' + length.toHHMMSS() + '</small>' +
                 '</div>' +
                 '<div class="d-flex w-100 justify-content-between">' +
-                    '<p class="mb-1">' + artist + '</h5>' +
+                    '<p class="mb-1">' + artist + '</p>' +
                     '<div class="listenOnSpotify">' + 
-                        '<button type="button" onclick=\'window.open(\'https://github.com/ColinToft/remember\', \'_blank\')\';><i class="fab fa-spotify"></i>&nbsp; Listen on Spotify</button>' +
+                        '<button type="button" onclick="window.open(\'https://open.spotify.com/track/' + id + '\', \'_blank\')";><i class="fab fa-spotify"></i>&nbsp; Listen on Spotify</button>' +
                     '</div>' +
                 '</div>' +
             '</a>';
